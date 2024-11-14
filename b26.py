54ea3755d270ca439ccf60fd9fe4d27d03361528
DevisHui <pinkzhuy221@gmail.com>
2024-11-14 22:56:36 +0700

    Update b26.py

--- b26.py
+++ b26.py
@@ -6,13 +6,16 @@ Created on Thu Nov 14 20:19:03 2024
 """
 
 #26 Tìm độ dài của chuỗi palindrome dài nhất có thể được tạo ra
-import collections
-def question_26(s: str):
-    demchuoi= collections.Counter(s)
-    length= 0
-    for count in demchuoi.values():
-        length += count // 2 * 2
-        if count % 2 == 1:
-            length += 1           
-    return length
-print(question_26(`))
+from collections import Counter
+def question_26(s: str)-> int:
+    demchuoi = Counter(s)
+    dodai = 0
+    cokitule = False
+    for dem in demchuoi.values():
+        dodai += dem // 2*2
+        if dem % 2 ==1:
+            cokitule = True
+    if cokitule:
+        dodai +=1
+    return dodai
+print(question_26("abccccdd"))
